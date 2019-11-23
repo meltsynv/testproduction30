017484807531929de2f22cc1eefe924351fa2af7
Viktor Meltsyn <meltsynv@yahoo.com>
2019-11-23 23:33:23 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.sass';
 
 import { Switch, Route} from "react-router-dom";
 
+//Components import
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import ScrollToTopBtn from './components/ScrollToTopBtn';
+
 //Pages
 import Home from './Pages/Index'
 import Events from './Pages/Events'
@@ -14,6 +19,7 @@ import Wir from './Pages/Wir'
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/wir" component={Wir} />
@@ -22,6 +28,8 @@ function App() {
         <Route path="/kontakt" component={Kontakt} />
         <Route component={ErrorPage} />
       </Switch>
+      <ScrollToTopBtn />
+      <Footer />
     </div>
   );
 }
