cd8daa84642c9c872bf0422ee41e379bf7352d92
Viktor Meltsyn <meltsynv@yahoo.com>
2019-11-23 22:45:53 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Switch, Route} from "react-router-dom";
 
 //Pages
 import Home from './Pages/Index'
+import Wir from './Pages/Wir'
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/wir" component={Wir}/>
       </Switch>
     </div>
   );
---
+++ src/Pages/Wir.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Wir(props) {
+    return (
+        <div>
+            Wir   
+        </div>
+    );
+}
+
+export default Wir;
