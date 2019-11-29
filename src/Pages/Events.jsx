a2a562b630c40a864a16a31a158710a8de5d593c
meltsynv <meltynv@yahoo.com>
2019-11-29 16:17:06 +0100

    change wir and events texte

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -32,7 +32,7 @@ function Events(props) {
                     <Col xs={12} md={6}>
                         <h1>Gutscheine</h1>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Sie möchten einen Gutschein verschenken? Ein perfektes Geschenk für jeden Anlass. Bitte fragen Sie uns, wir helfen Ihnen gerne weiter.
                         </p>
                     </Col>
                 </Row>
