4fa9cb6b3b061d7c98c04a97948471bc7f168599
Viktor Meltsyn <meltsynv@yahoo.com>
2019-12-12 17:02:52 +0100

    fixing navlink to behave like react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.sass';
-import './App-mobile.sass'
+import './App-mobile.sass';
 
-import { Switch, Route} from "react-router-dom";
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 //Components import
 import NavBar from './components/NavBar';
@@ -10,28 +10,30 @@ import Footer from './components/Footer';
 import ScrollToTopBtn from './components/ScrollToTopBtn';
 
 //Pages
-import Home from './Pages/Index'
-import Events from './Pages/Events'
-import ErrorPage from './Pages/ErrorPage'
-import Kontakt from './Pages/Kontakt'
-import Speisekarte from './Pages/Speisekarte'
-import Wir from './Pages/Wir'
+import Home from './Pages/Index';
+import Events from './Pages/Events';
+import ErrorPage from './Pages/ErrorPage';
+import Kontakt from './Pages/Kontakt';
+import Speisekarte from './Pages/Speisekarte';
+import Wir from './Pages/Wir';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/wir" component={Wir} />
-        <Route path="/speisekarte" component={Speisekarte}/>
-        <Route path="/events" component={Events}/>
-        <Route path="/kontakt" component={Kontakt} />
-        <Route component={ErrorPage} />
-      </Switch>
-      <ScrollToTopBtn />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/wir" component={Wir} />
+          <Route path="/speisekarte" component={Speisekarte} />
+          <Route path="/events" component={Events} />
+          <Route path="/kontakt" component={Kontakt} />
+          <Route component={ErrorPage} />
+        </Switch>
+        <ScrollToTopBtn />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import Logo from '../resources/logo.svg';
 import Instagram from '../resources/instagram.svg';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 import Layout from './Layout';
 
 const NavBar = () => {
@@ -14,12 +15,12 @@ const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/wir">Wir</Nav.Link>
-                            <Nav.Link href="/speisekarte">Speisekarte</Nav.Link>
-                            <Nav.Link href="/events">Events</Nav.Link>
-                            <Nav.Link href="/kontakt">Kontakt</Nav.Link>
-                            <Nav.Link href="https://www.instagram.com/steakhaus_eldorado/?hl=de" target='_blank'><img src={Instagram} width='24px' height='24px' /></Nav.Link>
+                            <NavLink className='nav-link' exact to="/">Home</NavLink>
+                            <NavLink className='nav-link' to="/wir">Wir</NavLink>
+                            <NavLink className='nav-link' to="/speisekarte">Speisekarte</NavLink>
+                            <NavLink className='nav-link' to="/events">Events</NavLink>
+                            <NavLink className='nav-link' to="/kontakt">Kontakt</NavLink>
+                            <NavLink className='nav-link' to="https://www.instagram.com/steakhaus_eldorado/?hl=de" target='_blank'><img src={Instagram} width='24px' height='24px' /></NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
