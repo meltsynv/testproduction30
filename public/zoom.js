0959e6ec9762b321f5096b0033858d2a37db1d72
Viktor Meltsyn <meltsynv@yahoo.com>
2020-01-29 14:47:12 +0100

    zoomen mobile anpassen, impressum angepasst

--- public/zoom.js
+++ public/zoom.js
@@ -4,9 +4,24 @@ document.addEventListener(
     if (event.scale !== 1) {
       event.preventDefault();
     }
-    else{
-        event.preventDefault();
-    }
   },
   false
 );
+
+document.addEventListener("gesturestart", function(e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gesturechange", function(e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gestureend", function(e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
+});
