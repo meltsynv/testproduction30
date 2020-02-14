2f951a307341799df2d26fcbbc46d80ed4b6b514
Viktor Meltsyn <meltsynv@yahoo.com>
2020-02-14 15:51:16 +0100

    Httos redirect

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import HttpsRedirect from "react-https-redirect";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
-import ScrollToTop from './components/scrollToTop';
+import ScrollToTop from "./components/scrollToTop";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
-  </BrowserRouter>,
+  <HttpsRedirect>
+    <BrowserRouter>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </BrowserRouter>
+  </HttpsRedirect>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
